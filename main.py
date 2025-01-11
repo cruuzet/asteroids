27f12bead5f0efc20056377f04f1e4c9b9da87d3
cruuzet <filip.krusell@gmail.com>
2025-01-11 15:23:20 +0100

    Finished boot-dev asteroids, time to upgrade!

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 import pygame
+import sys
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (updatable, drawable, shots)
+
+    asteroid_field = AsteroidField()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -12,7 +32,23 @@ def main():
                 return
 
         screen.fill("#000000")
+
+        for updatables in updatable:
+            updatables.update(dt)
+        for asteroid in asteroids:
+            if asteroid.check_collision(player) == True:
+                print('Game Over! MADDAFACK!')
+                sys.exit()
+            for shot in shots:
+                if asteroid.check_collision(shot) == True:
+                    asteroid.split()
+                    shot.kill()
+        for drawables in drawable:
+            drawables.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
